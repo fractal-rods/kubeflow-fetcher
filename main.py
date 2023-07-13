7d9534092b95361a130abc817a79b7d484b25f57
Jere Lotvonen <jere.lotvonen@gmail.com>
2023-07-13 17:07:03 +0300

    feat: add component creation scripts

--- main.py
+++ main.py
@@ -6,16 +6,25 @@ HOST = config("KUBEFLOW_ENDPOINT")
 USER = config("KUBEFLOW_USERNAME")
 PASS = config("KUBEFLOW_PASSWORD")
 
-auth_session = get_istio_auth_session(
-    url=HOST,
-    username=USER,
-    password=PASS
-)
+if __name__ == "__main__":
+    # Get cookie from authorization
+    auth_session = get_istio_auth_session(
+        url=HOST,
+        username=USER,
+        password=PASS
+    )
 
-print("session: ", auth_session)
+    print("session: ", auth_session)
 
-client = kfp.Client(host=f"{HOST}/pipeline", ssl_ca_cert="mycertfile.pem", cookies=auth_session["session_cookie"])
-print("health: ", client.get_kfp_healthz())
-print("list exps: ", client.list_experiments(namespace="oulu-profile"))
-print("so far so good!")
-print("list of pipelines: ", client.list_pipelines())
+    # Connect the kubeflow client
+    client = kfp.Client(
+        host=f"{HOST}/pipeline",
+        ssl_ca_cert="mycertfile.pem", 
+        cookies=auth_session["session_cookie"]
+        )
+
+    client.upload_pipeline(
+        pipeline_name="Addition pipeline",
+        description="Pipeline for testing connection and SDK functionality",
+        pipeline_package_path="pipelines/add_pipeline.yaml"
+        )
