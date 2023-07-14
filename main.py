76c79cd9af2b945fe299148151dbf2ed8f906a04
Jere Lotvonen <jere.lotvonen@gmail.com>
2023-07-14 14:50:58 +0300

    run pre-commit hooks

--- main.py
+++ main.py
@@ -8,23 +8,19 @@ PASS = config("KUBEFLOW_PASSWORD")
 
 if __name__ == "__main__":
     # Get cookie from authorization
-    auth_session = get_istio_auth_session(
-        url=HOST,
-        username=USER,
-        password=PASS
-    )
+    auth_session = get_istio_auth_session(url=HOST, username=USER, password=PASS)
 
     print("session: ", auth_session)
 
     # Connect the kubeflow client
     client = kfp.Client(
         host=f"{HOST}/pipeline",
-        ssl_ca_cert="mycertfile.pem", 
-        cookies=auth_session["session_cookie"]
-        )
+        ssl_ca_cert="mycertfile.pem",
+        cookies=auth_session["session_cookie"],
+    )
 
     client.upload_pipeline(
         pipeline_name="Addition pipeline",
         description="Pipeline for testing connection and SDK functionality",
-        pipeline_package_path="pipelines/add_pipeline.yaml"
-        )
+        pipeline_package_path="pipelines/add_pipeline.yaml",
+    )
