e8749a73c9bea1e82eca24aa320869fe6244d75d
Jere Lotvonen <jere.lotvonen@gmail.com>
2023-08-04 18:17:25 +0300

    add .ruff.toml

--- src/pipeline.py
+++ src/pipeline.py
@@ -3,7 +3,7 @@ from kfp import dsl
 import kfp.components as comp
 
 # components need to be loaded before pipeline definition not inside the pipeline!
-lake_op = comp.load_component_from_file("lakefs/component.yaml")
+lake_op = comp.load_component_from_file("component.yaml")
 
 
 @dsl.pipeline(
@@ -15,8 +15,6 @@ def lakefs_modify_pipeline(
     lake_token: str = "",
 ):
     modify_data_step = lake_op(host=lake_host, id=lake_id, token=lake_token)
-
-    return modify_data_step
 
 
 if __name__ == "__main__":
